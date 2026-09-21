3def4d35a57f18853f21797229e8635abc57171b
Hagen Schupp <hagen.schupp@rwth-aachen.de>
2026-09-22 00:53:56 +0200

    Adding binary message serialization vs JSON comparison

--- src/main/java/com/brakid/binary/app/App.java
+++ src/main/java/com/brakid/binary/app/App.java
@@ -3,17 +3,25 @@ package com.brakid.binary.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.brakid.binary.serialization.UserSerializer;
+import com.brakid.binary.serialization.MessageSerializer;
+import com.brakid.binary.serialization.Serializer;
+import com.brakid.binary.types.Message;
 import com.brakid.binary.types.User;
+
+import tools.jackson.databind.json.JsonMapper;
 
 public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-        User user = new User(123l, "Test");
-        UserSerializer userSerializer = new UserSerializer();
-        byte[] bytes = userSerializer.serialize(user);
-        LOGGER.info("Bytes: {}", bytes);
-        User user1 = userSerializer.deserialize(bytes);
-        LOGGER.info("User: {}", user1);
+        User sender = new User(Long.MAX_VALUE, "Sender");
+        User receiver = new User(Long.MIN_VALUE, "receiver");
+        Message message = new Message(Long.MAX_VALUE, "Some message with a bit of text", sender, receiver, Long.MAX_VALUE);
+        Serializer<Message> messageSerializer = new MessageSerializer();
+        byte[] bytes = messageSerializer.serialize(message);
+        LOGGER.info("Bytes: {}, length: {}", bytes, bytes.length);
+    
+        JsonMapper jsonMapper = new JsonMapper();
+        String json = jsonMapper.writeValueAsString(message);
+        LOGGER.info("Json: {}, length: {}", json, json.length());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.brakid.binary {
     requires com.google.common;
     requires org.slf4j;
+    requires tools.jackson.databind;
 
     exports com.brakid.binary.app;
 }
